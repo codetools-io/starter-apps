d4978c44785207ee04fee5c66c3f079d05ad09c8
Cliff Pyles <cliff@cliffpyles.com>
2020-08-15 09:23:46 -0400

    Add nav items for apps

--- src/config/nav.js
+++ src/config/nav.js
@@ -1,3 +1,18 @@
 export const nav = {
-  routes: [{ key: 'dashboard', path: '/dashboard', label: 'Dashboard' }],
+  routes: [
+    { key: 'calendar', label: 'Calendar', path: '/calendar' },
+    { key: 'chat', label: 'Chat', path: '/chat' },
+    { key: 'commerce', label: 'Commerce', path: '/commerce' },
+    { key: 'contacts', label: 'Contacts', path: '/contacts' },
+    { key: 'dashboard', label: 'Dashboard', path: '/dashboard' },
+    { key: 'email', label: 'Email', path: '/email' },
+    { key: 'notes', label: 'Notes', path: '/notes' },
+    { key: 'profile', label: 'Profile', path: '/profile' },
+    {
+      key: 'projectManager',
+      label: 'Project Manager',
+      path: '/project-manager',
+    },
+    { key: 'tasks', label: 'Tasks', path: '/tasks' },
+  ],
 };
