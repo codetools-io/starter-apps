17d631c3d62962983ec8ffb20b94ce03826e9fa0
Cliff Pyles <cliff@cliffpyles.com>
2020-09-02 07:56:32 -0400

    Make inboxes in Email scrollable

--- src/containers/Email/Email.js
+++ src/containers/Email/Email.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import moment from 'moment';
-import { Avatar, Box, Button, Card, Heading, Text, TextInput } from 'grommet';
+import {
+  Avatar,
+  Box,
+  Button,
+  Card,
+  Grid,
+  Heading,
+  InfiniteScroll,
+  Text,
+  TextInput,
+} from 'grommet';
 import { StatusGoodSmall } from 'grommet-icons';
-import ThreadLayout from 'components/ThreadLayout';
-import ColumnLayout from 'components/ColumnLayout';
 
 import useEmail from './useEmail';
 
@@ -20,7 +28,7 @@ function EmailShort({
   ...props
 }) {
   return (
-    <Box direction="row" pad="medium" gap="small" {...props}>
+    <Box direction="row" pad="medium" gap="small" flex={false} {...props}>
       <Avatar src={sender?.profile} />
       <Box flex>
         <Box>
@@ -73,86 +81,112 @@ export default function () {
 
   return (
     <Box pad="medium" fill>
-      <Card background="white">
-        <ThreadLayout>
-          <ColumnLayout gridArea="menu">
+      <Card background="white" fill>
+        <Grid
+          columns={['1/4', '1/4', '1/4', '1/4']}
+          rows={['auto', 'flex', 'auto']}
+          areas={[
+            [
+              'EmailCompose',
+              'EmailInboxHeader',
+              'EmailThreadHeader',
+              'EmailThreadHeader',
+            ],
+            [
+              'EmailSidebar',
+              'EmailInboxMain',
+              'EmailThreadMain',
+              'EmailThreadMain',
+            ],
+            [
+              'EmailSidebar',
+              'EmailInboxMain',
+              'EmailThreadMain',
+              'EmailThreadMain',
+            ],
+          ]}
+          fill
+        >
+          <Box
+            gridArea="EmailCompose"
+            pad="medium"
+            border={[{ side: 'bottom' }, { side: 'right' }]}
+            height="xsmall"
+            justify="center"
+          >
+            <Button label="Compose" primary />
+          </Box>
+          <Box gridArea="EmailSidebar" border="right">
+            <Box pad="medium" gap="small">
+              {Object.entries(emailsByFolder).map(([folder, emails]) => (
+                <Box key={folder} direction="row" justify="between">
+                  <Text>{folder}</Text>
+                  <Text>{emails?.length}</Text>
+                </Box>
+              ))}
+            </Box>
+            <Box pad="medium" gap="small">
+              {labels.map((label) => (
+                <Box key={label?.id} direction="row" justify="between">
+                  <StatusGoodSmall color={label?.color} />
+                  <Text>{label?.name}</Text>
+                </Box>
+              ))}
+            </Box>
+          </Box>
+          <Box
+            gridArea="EmailInboxHeader"
+            direction="row"
+            pad="medium"
+            border={[{ side: 'bottom' }, { side: 'right' }]}
+            height="xsmall"
+            align="center"
+          >
+            <Heading level="4" margin="none">
+              {activeFolder}
+            </Heading>
+          </Box>
+
+          <Box gridArea="EmailInboxMain" border="right">
             <Box
-              gridArea="header"
-              pad="medium"
-              border={[{ side: 'bottom' }, { side: 'right' }]}
-              height="xsmall"
-              justify="center"
+              pad={{ vertical: 'xsmall', horizontal: 'small' }}
+              border="bottom"
+              flex={false}
             >
-              <Button label="Compose" primary />
+              <TextInput placeholder="Search Email" plain />
             </Box>
-            <Box gridArea="main" border="right">
-              <Box pad="medium" gap="small">
-                {Object.entries(emailsByFolder).map(([folder, emails]) => (
-                  <Box key={folder} direction="row" justify="between">
-                    <Text>{folder}</Text>
-                    <Text>{emails?.length}</Text>
-                  </Box>
-                ))}
-              </Box>
-              <Box pad="medium" gap="small">
-                {labels.map((label) => (
-                  <Box key={label?.id} direction="row" justify="between">
-                    <StatusGoodSmall color={label?.color} />
-                    <Text>{label?.name}</Text>
-                  </Box>
-                ))}
-              </Box>
-            </Box>
-          </ColumnLayout>
-          <ColumnLayout gridArea="threads">
-            <Box
-              gridArea="header"
-              direction="row"
-              pad="medium"
-              border={[{ side: 'bottom' }, { side: 'right' }]}
-              height="xsmall"
-              align="center"
-            >
-              <Heading level="4" margin="none">
-                {activeFolder}
-              </Heading>
-            </Box>
-            <Box gridArea="main" gap="small" border="right">
-              <Box
-                pad={{ vertical: 'xsmall', horizontal: 'small' }}
-                border="bottom"
+            <Box overflow="auto">
+              <InfiniteScroll
+                steps={3}
+                items={emailsByFolder[activeFolder]}
+                show={0}
               >
-                <TextInput placeholder="Search Email" plain />
-              </Box>
-              <Box>
-                {emailsByFolder[activeFolder].map((email, index) => (
+                {(email, index) => (
                   <EmailShort
                     key={email.id}
                     {...email}
                     labels={email.labels.map((label) => labelsById[label])}
                     border={index ? 'top' : null}
                   />
-                ))}
-              </Box>
+                )}
+              </InfiniteScroll>
             </Box>
-          </ColumnLayout>
-          <ColumnLayout gridArea="thread">
-            <Box
-              gridArea="header"
-              direction="row"
-              justify="between"
-              height="xsmall"
-              pad="medium"
-              border="bottom"
-            >
-              <Box>actions</Box>
-              <Box>toolbar</Box>
-            </Box>
-            <Box gridArea="main" pad="medium">
-              sidebar main
-            </Box>
-          </ColumnLayout>
-        </ThreadLayout>
+          </Box>
+          <Box
+            gridArea="EmailThreadHeader"
+            direction="row"
+            justify="between"
+            height="xsmall"
+            pad="medium"
+            border="bottom"
+          >
+            <Box>actions</Box>
+            <Box>toolbar</Box>
+          </Box>
+          <Box gridArea="EmailThreadMain" pad="medium">
+            <Box>sidebar main</Box>
+          </Box>
+        </Grid>
       </Card>
     </Box>
   );
