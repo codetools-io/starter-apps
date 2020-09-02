aac15ddd91a26c70492f943cd98cbe5a572adf46
Cliff Pyles <cliff@cliffpyles.com>
2020-09-02 06:36:16 -0400

    Fix Cart overflow issue

--- src/containers/Commerce/Cart/Cart.js
+++ src/containers/Commerce/Cart/Cart.js
@@ -22,83 +22,85 @@ export default function Cart({ children, ...props }) {
 
   return (
     <Box className="Cart" pad="medium" fill>
-      <Card background="white" pad="medium">
-        <Table>
-          <TableHeader>
-            <TableRow>
-              <TableCell scope="col"></TableCell>
-              <TableCell scope="col"></TableCell>
-              <TableCell scope="col">
-                <Text weight="bold">Product</Text>
-              </TableCell>
-              <TableCell scope="col">
-                <Text weight="bold">Price</Text>
-              </TableCell>
-              <TableCell scope="col">
-                <Text weight="bold">Quantity</Text>
-              </TableCell>
-              <TableCell scope="col">
-                <Text weight="bold">Total</Text>
-              </TableCell>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {lineItems.map((lineItem) => (
-              <TableRow key={`cart-line-item-${lineItem.id}`}>
-                <TableCell scope="row">
-                  <Button
-                    icon={<FormClose />}
-                    onClick={() => removeProduct(lineItem.id)}
-                  />
+      <div>
+        <Card background="white" pad="medium">
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableCell scope="col"></TableCell>
+                <TableCell scope="col"></TableCell>
+                <TableCell scope="col">
+                  <Text weight="bold">Product</Text>
                 </TableCell>
-                <TableCell scope="row">
-                  <Box height="small" width="small">
-                    <Image src={lineItem.image} fit="contain" />
-                  </Box>
+                <TableCell scope="col">
+                  <Text weight="bold">Price</Text>
                 </TableCell>
-                <TableCell scope="row">
-                  <Text weight="bold">{lineItem.title}</Text>
-                  <Paragraph color="dark-6">{lineItem.description}</Paragraph>
+                <TableCell scope="col">
+                  <Text weight="bold">Quantity</Text>
                 </TableCell>
-                <TableCell scope="row">
-                  <Text weight="bold">{lineItem.priceLabel}</Text>
-                </TableCell>
-                <TableCell scope="row">
-                  <Box width="xsmall">
-                    <TextInput
-                      type="number"
-                      value={lineItem.quantity}
-                      onChange={(event) => {
-                        updateProduct({
-                          id: lineItem.id,
-                          quantity: event.target.value,
-                        });
-                      }}
-                    />
-                  </Box>
-                </TableCell>
-                <TableCell scope="row">
-                  <Text weight="bold">{lineItem.totalLabel}</Text>
+                <TableCell scope="col">
+                  <Text weight="bold">Total</Text>
                 </TableCell>
               </TableRow>
-            ))}
-          </TableBody>
-          <TableFooter>
-            <TableRow>
-              <TableCell></TableCell>
-              <TableCell></TableCell>
-              <TableCell></TableCell>
-              <TableCell></TableCell>
-              <TableCell align="right">
-                <Text weight="bold">Total</Text>
-              </TableCell>
-              <TableCell>
-                <Text weight="bold">{totalLabel}</Text>
-              </TableCell>
-            </TableRow>
-          </TableFooter>
-        </Table>
-      </Card>
+            </TableHeader>
+            <TableBody>
+              {lineItems.map((lineItem) => (
+                <TableRow key={`cart-line-item-${lineItem.id}`}>
+                  <TableCell scope="row">
+                    <Button
+                      icon={<FormClose />}
+                      onClick={() => removeProduct(lineItem.id)}
+                    />
+                  </TableCell>
+                  <TableCell scope="row">
+                    <Box height="small" width="small">
+                      <Image src={lineItem.image} fit="contain" />
+                    </Box>
+                  </TableCell>
+                  <TableCell scope="row">
+                    <Text weight="bold">{lineItem.title}</Text>
+                    <Paragraph color="dark-6">{lineItem.description}</Paragraph>
+                  </TableCell>
+                  <TableCell scope="row">
+                    <Text weight="bold">{lineItem.priceLabel}</Text>
+                  </TableCell>
+                  <TableCell scope="row">
+                    <Box width="xsmall">
+                      <TextInput
+                        type="number"
+                        value={lineItem.quantity}
+                        onChange={(event) => {
+                          updateProduct({
+                            id: lineItem.id,
+                            quantity: event.target.value,
+                          });
+                        }}
+                      />
+                    </Box>
+                  </TableCell>
+                  <TableCell scope="row">
+                    <Text weight="bold">{lineItem.totalLabel}</Text>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+            <TableFooter>
+              <TableRow>
+                <TableCell></TableCell>
+                <TableCell></TableCell>
+                <TableCell></TableCell>
+                <TableCell></TableCell>
+                <TableCell align="right">
+                  <Text weight="bold">Total</Text>
+                </TableCell>
+                <TableCell>
+                  <Text weight="bold">{totalLabel}</Text>
+                </TableCell>
+              </TableRow>
+            </TableFooter>
+          </Table>
+        </Card>
+      </div>
     </Box>
   );
 }
