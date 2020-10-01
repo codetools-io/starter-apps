36a219a0f049b2715cb4470a78fad895557c7d23
Cliff Pyles <cliff@cliffpyles.com>
2020-10-01 07:00:23 -0400

    Update Home snapshot

--- src/internal/components/Home/Home.test.js
+++ src/internal/components/Home/Home.test.js
@@ -1,15 +1,17 @@
-import React from 'react'
-import Home from './Home'
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Home from './Home';
 
 describe('Home', () => {
   test('can render', () => {
     const { container } = renderComponent(
-      <Home>
-        <p>Content for the Home</p>
-      </Home>
-    )
+      <BrowserRouter>
+        <Home>
+          <p>Content for the Home</p>
+        </Home>
+      </BrowserRouter>
+    );
 
-    expect(container).toMatchSnapshot()
-  })
-})
-
+    expect(container).toMatchSnapshot();
+  });
+});
