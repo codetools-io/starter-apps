a82712e675677b15a360562444c2cf9a198d30ed
Cliff Pyles <cliff@cliffpyles.com>
2020-10-01 14:27:18 -0400

    Integrate analytics

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Amplify from 'aws-amplify';
 import { BrowserRouter } from 'react-router-dom';
 
 import Router from 'Router';
 import AppShell from 'internal/components/AppShell';
 import ErrorBoundary from 'internal/components/ErrorBoundary';
 
+import awsconfig from './aws-exports';
+
+Amplify.configure(awsconfig);
 export default function App() {
   return (
     <ErrorBoundary>
