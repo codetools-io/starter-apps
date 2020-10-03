405d98f25f746e36cc9555bed1d26361ec7b174d
Cliff Pyles <cliff@cliffpyles.com>
2020-10-03 04:15:41 -0400

    Add app blueprint

---
+++ .blueprints/app/files/src/apps/__blueprintInstance__/use__blueprintInstance__.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Box } from 'grommet';
+
+export default function {{blueprintInstance}} ({ children, ...props }) {
+
+  return <Box className="{{blueprintInstance}}" {...props}>{children}</Box>;
+};
+
+
+
