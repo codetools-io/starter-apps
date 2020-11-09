06428138e7404cc8eb13ab37b235c97afb7aa43b
Cliff Pyles <cliff@cliffpyles.com>
2020-11-09 06:12:15 -0500

    Add about and contact page

--- src/internal/pages/About/About.js
+++ src/internal/pages/About/About.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Box, Card, Paragraph } from 'grommet';
+import ContactPlatforms from 'internal/components/ContactPlatforms';
 import PageHeader from 'internal/components/PageHeader';
 
 export default function About({ children, ...props }) {
@@ -8,15 +9,20 @@ export default function About({ children, ...props }) {
       <PageHeader title="About" />
 
       <Card pad="large" gap="medium" background="white">
-        <Paragraph
-          margin={{ horizontal: 'auto' }}
-          size="xlarge"
-          textAlign="center"
-        >
-          StarterApps is a resource for solutions to building Grommet based
-          applications. We provide developers with examples of how real-world
-          applications can be built with Grommet.
-        </Paragraph>
+        <Box height="medium" align="center" direction="center">
+          <Paragraph
+            margin={{ horizontal: 'auto' }}
+            size="xlarge"
+            textAlign="center"
+          >
+            StarterApps is a resource for solutions to building Grommet based
+            applications. We provide developers with examples of how real-world
+            applications can be built with Grommet.
+          </Paragraph>
+        </Box>
+        <Box pad={{ bottom: 'xlarge' }}>
+          <ContactPlatforms />
+        </Box>
       </Card>
     </Box>
   );
